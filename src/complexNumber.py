02c5c1ca61e73484607cb1e5d74584589e3c090a
suelz <conorpadraigkennedy@gmail.com>
2018-10-11 19:28:25 -0500

    just started finish tomm

---
+++ src/complexNumber.py
@@ -0,0 +1,15 @@
+
+class complexNumber:
+
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def getReal(self):
+        return self.real
+
+    def getImag(self):
+        return self.imag
+
+
+
---
+++ src/driver.py
@@ -0,0 +1,10 @@
+
+from complexNumber import *
+
+def main():
+   x = complexNumber(1, 2)
+   print (x.getReal())
+   print (x.getImag())
+
+if __name__ == "__main__":
+    main()
